2747272d2a18759c51d6ef5ad70ca45ba75f8fca
takashi oka <takashi.oka@gmail.com>
2021-06-05 02:11:04 +0900

    #20 コメント削除機能にエラーがあったので修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,36 @@
 class CommentsController < ApplicationController
+  before_action :set_game, only: %i(create edit update)
+
   def create
     @game = Game.find(params[:game_id])
-    @comment = @game.comments.build(comment_params)
+    @comment = @game.comments.build(permitted_parameter)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to game_path(@game) }
+        # format.html { redirect_to game_path(@game) }
+        format.js { render :index }
       else
         format.html { redirect_to game_path(@game), notice: '投稿できませんでした' }
+      end
+    end
+  end
+
+  def edit
+    @comment = @game.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメント編集中'
+      format.js{ render :edit }
+    end
+  end
+
+  def update
+    @comment = @game.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(permitted_parameter)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index}
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js {render :edit_error}
       end
     end
   end
@@ -18,7 +42,10 @@ class CommentsController < ApplicationController
   end
 
   private
-  def comment_params
-    params.require(:comment).permit(:game_id, :content, :image, :image_cache).merge(user_id: current_user.id)
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+  def permitted_parameter
+    params.require(:comment).permit(:game_id, :content).merge(user_id: current_user.id)
   end
 end
