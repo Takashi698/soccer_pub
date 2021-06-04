de9fa8847ece8a5b77853a4be08f567060f0ba73
takashi oka <takashi.oka@gmail.com>
2021-06-04 22:33:02 +0900

    #18 サインアップの画面に移動出来ないエラーを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
 module ApplicationHelper
- 
+  def resource_name
+      :user
+  end
+
+  def resource
+      @resource ||= User.new
+  end
+
+  def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  # def text_placeholder
+  # <<-"EOS".strip_heredoc
+  #     タグをつけることができます。
+  #     複数つけたい場合は' , 'で区切ってください。
+  #     EOS
+  # end
 end
