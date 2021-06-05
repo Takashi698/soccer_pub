d8982f64eba16c1831c6a050c52df68f12cdd02b
takashi oka <takashi.oka@gmail.com>
2021-06-05 19:37:52 +0900

    #18 全体的な微調整

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,13 +2,42 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_fill: [300, 300, "Center"]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  
+  version :thumb100 do
+    process resize_to_fit: [100, 100]
+  end
 
+  version :thumb150 do
+    process resize_to_fit: [150, 150]
+  end
+
+  version :thumb200 do
+    process resize_to_fit: [200, 200]
+  end
+
+  version :thumb250 do
+    process resize_to_fit: [250, 250]
+  end
+
+  version :thumb300 do
+    process resize_to_fit: [300, 300]
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+
+  # storage :fog
+  # storage :file
+
+  def default_url(*args)
+    "default.png"
+  end
+
+  # アップロードファイルの保存先
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -30,13 +59,17 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+    # process resize_to_fill: [200, 200, "Center"]
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
     %w(jpg jpeg gif png)
+  end
+
+  def size_range
+    0..5.megabytes
   end
 
   # Override the filename of the uploaded files:
