59fe988321ba9e602b04a2278ec22953e2a30314
takashi oka <takashi.oka@gmail.com>
2021-06-05 21:44:19 +0900

    #16 サイドメニュー再設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,15 @@ module ApplicationHelper
       @devise_mapping ||= Devise.mappings[:user]
   end
 
+  def sidebar_link_item(name, path)
+    class_name = 'channel'
+    class_name << ' active' if current_page?(path)
+
+    content_tag :li, class:class_name do
+      link_to name, path, class: 'channel_name'
+    end
+  end
+
   # def text_placeholder
   # <<-"EOS".strip_heredoc
   #     タグをつけることができます。
