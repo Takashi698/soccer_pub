df0bd46fe9c0607b81f8922de5e13be873f54995
takashi oka <takashi.oka@gmail.com>
2021-06-13 19:55:28 +0900

    #33 system specテストの雛形を作成

---
+++ spec/systems/game_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe '試合ルーム関連機能', type: :system do
+  it '試合ルーム作成機能' do
+  end
+  it '試合ルーム編集機能' do
+  end
+  it '試合ルーム削除機能' do
+  end
+  it '試合ルームあいまい検索機能' do
+  end
+  it '試合ルームお気に入り機能' do
+  end
+  it 'コメント機能' do
+  end
+end
---
+++ spec/systems/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+RSpec.describe 'user関連機能', type: :system do
+  describe 'devise関連機能' do
+    it 'サインアップ機能' do
+    end
+    it 'ログイン機能' do
+    end
+  end
+  describe 'その他機能' do
+    it 'ゲストログイン機能' do
+    end
+    it 'お気に入り一覧機能' do
+    end
+    it '試合ルーム一覧機能' do
+    end
+    it 'フォロー機能' do
+    end
+  end
+end 
