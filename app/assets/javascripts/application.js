b99cc5abd6f08390bc0b6a7854e06c9785c04887
takashi oka <takashi.oka@gmail.com>
2021-06-19 23:43:47 +0900

    application.js, application.scss : bootstrap, jqueryの設定を変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.min
+//= require bootstrap-sprockets
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
