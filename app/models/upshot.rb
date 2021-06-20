4f07ed691eb95a58deaff15a53ed03c8a29c204a
takashi oka <takashi.oka@gmail.com>
2021-06-21 02:46:31 +0900

    #35 チーム得点のバリデーションを追加

--- app/models/upshot.rb
+++ app/models/upshot.rb
@@ -1,5 +1,7 @@
 class Upshot < ApplicationRecord
   belongs_to :game
+  validates :team_a_point, presence: true
+  validates :team_b_point, presence: true
   # belongs_to :team_a, foreign_key: :team_a_id, class_name: :team
   # belongs_to :team_b, foreign_key: :team_b_id, class_name: :team
 end
